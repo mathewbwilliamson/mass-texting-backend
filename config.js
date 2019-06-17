c028195b8b26741b90fb0f515af20343cc4813e4
Mathew Williamson <mwilliamson@64labs.com>
2019-06-16 22:30:48 -0400

    Added some extra Auth files

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 'use strict';
 
 module.exports = {
-  PORT: process.env.PORT || 8080,
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-  DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
-  TEST_DATABASE_URL:
-        process.env.TEST_DATABASE_URL ||
-        'mongodb://localhost/thinkful-backend-test'
+      PORT: process.env.PORT || 8080,
+      CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+      DATABASE_URL:
+            process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
+      TEST_DATABASE_URL:
+            process.env.TEST_DATABASE_URL ||
+            'mongodb://localhost/thinkful-backend-test',
+      APP_SECRET: process.env.APP_SECRET,
 };
